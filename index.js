e80b8297bb9f34584ab46a2009d8a7372a5ea5e9
Net-Informatica <appnetinformatica@Mac-mini-di-Alessandro.local>
2021-02-25 16:15:27 +0100

    update

--- index.js
+++ index.js
@@ -1,24 +1,87 @@
 require("dotenv").config();
+const bcrypt = require("bcrypt");
 const express = require("express");
 const app = express();
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
+
+const initializePassport = require("./passport.config");
+initializePassport(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
+
+const users = [];
+let id = 0;
 
 app.set("view-engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", { name: "Kyle" });
+app.get("/", checkAuth, (req, res) => {
+  res.render("index.ejs", { name: req.user.username });
 });
 
 app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
-app.post("/register", (req, res) => {
-  console.log(req.body.username);
-  console.log(req.body.password);
+app.post("/register", async (req, res) => {
+  let username = req.body.username;
+  let email = req.body.email;
+  let password = req.body.password;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    let newUser = {
+      id: id,
+      username: username,
+      email: email,
+      password: hashedPassword,
+    };
+    id++;
+    users.push(newUser);
+    console.log(users);
+    res.redirect("/login");
+  } catch (error) {
+    res.redirect("/register");
+  }
 });
+
+app.delete("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("/login");
+});
+
+function checkAuth(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
